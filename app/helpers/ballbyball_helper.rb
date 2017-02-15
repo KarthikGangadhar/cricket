82a69f6958f67334e9020b0d9b3eb7d00e5f1256
kgangadhar <karthikg1643@gmail.com>
2017-02-15 23:14:05 +0530

    added over details to ballbyball details of match

--- app/helpers/ballbyball_helper.rb
+++ app/helpers/ballbyball_helper.rb
@@ -1,10 +1,10 @@
 module BallbyballHelper
+  
   def getPlayerName(ballbyball, pid)
-
     player_name = ""
     ballbyball['data']['team'].each do |team|
       team['player'].each do |player|
-        if player['player_id'] == pid
+        if (player['player_id'] == pid || player['object_id'] == pid )
           player_name = player['known_as']
         end
       end
@@ -12,4 +12,17 @@ module BallbyballHelper
     return player_name
 
   end
+  
+  def getTeamName(ballbyball, tid)
+
+    team_name = ""
+    ballbyball['data']['team'].each do |team|
+        if team['team_id'] == tid
+          team_name = team['team_name']
+        end
+    end
+    return team_name
+
+  end
+  
 end
